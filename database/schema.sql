a2a49351698ba8d92cb807ceecff0b57d49b2804
Amanda <amanda.beatryz@ufrpe.br>
2026-05-20 21:08:07 -0300

    atualizando schema

--- database/schema.sql
+++ database/schema.sql
@@ -25,58 +25,58 @@ CREATE INDEX idx_disciplinas_periodo ON disciplinas(periodo);
 -- 1º PERÍODO
 -- =========================
 INSERT INTO disciplinas (codigo, nome, periodo, carga_horaria) VALUES
-('14734', 'Sustentabilidade em Sistemas de Informação', 1, 60),
-('14708', 'Fundamentos Matemáticos para Sistemas de Informação I', 1, 60),
-('14733', 'Projeto Interdisciplinar para Sistemas de Informação I', 1, 60),
-('14709', 'Princípios de Programação', 1, 60),
+('14734', 'Sustentabilidade em Sistemas de Informação', 1, 60);
+('14708', 'Fundamentos Matemáticos para Sistemas de Informação I', 1, 60);
+('14733', 'Projeto Interdisciplinar para Sistemas de Informação I', 1, 60);
+('14709', 'Princípios de Programação', 1, 60);
 ('4109', 'Introdução à Administração', 1, 60);
 
 -- =========================
 -- 2º PERÍODO
 -- =========================
 INSERT INTO disciplinas (codigo, nome, periodo, carga_horaria) VALUES
-('4162', 'Fundamentos de Sistemas de Informação II', 2, 60),
-('14378', 'Projeto Interdisciplinar de Sistemas de Informação II', 2, 60),
-('14737', 'Fundamentos Matemáticos para Sistemas de Informação II', 2, 60),
-('14735', 'Elementos de Sistemas Computacionais', 2, 60),
+('4162', 'Fundamentos de Sistemas de Informação II', 2, 60);
+('14378', 'Projeto Interdisciplinar de Sistemas de Informação II', 2, 60);
+('14737', 'Fundamentos Matemáticos para Sistemas de Informação II', 2, 60);
+('14735', 'Elementos de Sistemas Computacionais', 2, 60);
 ('14736', 'Fundamentos de Problemas Computacionais I', 2, 60);
 
 -- =========================
 -- 3º PERÍODO
 -- =========================
 INSERT INTO disciplinas (codigo, nome, periodo, carga_horaria) VALUES
-('14340', 'Engenharia para Sistemas de Informação I', 3, 60),
-('14341', ' Introdução ao Armazenamento e Análise de Dados', 3, 60),
-('14342', 'Projeto Interdisciplinar para Sistemas de Informação III', 3, 60),
-('14379', 'Desenvolvimento de Sistemas de Informação', 3, 60),
+('14340', 'Engenharia para Sistemas de Informação I', 3, 60);
+('14341', ' Introdução ao Armazenamento e Análise de Dados', 3, 60);
+('14342', 'Projeto Interdisciplinar para Sistemas de Informação III', 3, 60);
+('14379', 'Desenvolvimento de Sistemas de Informação', 3, 60);
 
 -- =========================
 -- 4º PERÍODO
 -- =========================
 INSERT INTO disciplinas (codigo, nome, periodo, carga_horaria) VALUES
-('14344', 'Princípios de Software Básico', 4, 60),
-('14346', 'Projeto Interdisciplinar de Sistemas de Informação IV', 4, 60),
-('14343', 'Fundamentos de Problemas Computacionais II', 4, 60),
-('14345', 'Sistemas de Informação na Internet', 4, 60),
+('14344', 'Princípios de Software Básico', 4, 60);
+('14346', 'Projeto Interdisciplinar de Sistemas de Informação IV', 4, 60);
+('14343', 'Fundamentos de Problemas Computacionais II', 4, 60);
+('14345', 'Sistemas de Informação na Internet', 4, 60);
 
 -- =========================
 -- 5º PERÍODO
 -- =========================
 INSERT INTO disciplinas (codigo, nome, periodo, carga_horaria) VALUES
-('14347', 'Engenharia para Sistemas de Informação II', 5, 60),
-('14348', 'Projeto de Desenvolvimento Tecnológico para o Mundo I', 5, 60),
-('06299', 'Segurança e Auditoria de Sistemas', 5, 60),
-('4249', 'Empreendedorismo e Inovação', 5, 60),
+('14347', 'Engenharia para Sistemas de Informação II', 5, 60);
+('14348', 'Projeto de Desenvolvimento Tecnológico para o Mundo I', 5, 60);
+('06299', 'Segurança e Auditoria de Sistemas', 5, 60);
+('4249', 'Empreendedorismo e Inovação', 5, 60);
 
 
 -- =========================
 -- 6º PERÍODO
 -- =========================
 INSERT INTO disciplinas (codigo, nome, periodo, carga_horaria) VALUES
-('14352', 'Engenharia para Sistemas de Informação III', 6, 60),
-('14351 ', 'Modelagem de Dados', 6, 60),
+('14352', 'Engenharia para Sistemas de Informação III', 6, 60);
+('14351 ', 'Modelagem de Dados', 6, 60);
 ('14350', 'Projeto de Desenvolvimento Tecnológico para o Mundo II', 6, 60);
-('6507', 'Cálculo N1', 6, 60);,
+('6507', 'Cálculo N1', 6, 60);
 
 -- =========================
 -- 7º PERÍODO
